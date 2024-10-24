08567ba06fcbaa67c87ccc13fa0488c8206d26b3
wingyou <winwing.you@gmail.com>
2024-10-25 08:57:23 +0900

    feat(tester_v2): update employee test

--- tester_v2/src/test/java/com/example/tester_v2/repository/EmployeeRepositoryTest.java
+++ tester_v2/src/test/java/com/example/tester_v2/repository/EmployeeRepositoryTest.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.tester_v2.annotation.CustomTest;
 import com.example.tester_v2.entity.Employee;
-import com.example.tester_v2.entity.Salary;
-import com.example.tester_v2.entity.Title;
 import com.example.tester_v2.etc.Gender;
 
 /**
@@ -36,16 +34,6 @@ public class EmployeeRepositoryTest {
     @Transactional
     void new_employee() {
         // Given
-        Title title = Title.builder()
-                .title("Senior Engineer")
-                .fromDate(LocalDate.of(1986, 6, 26))
-                .toDate(LocalDate.of(9999, 1, 1))
-                .build();
-        Salary salary = Salary.builder()
-                .salary(60117)
-                .fromDate(LocalDate.of(1986, 6, 26))
-                .toDate(LocalDate.of(1987, 6, 26))
-                .build();
         Employee employee = Employee.builder()
                 .employeeNumber(10001)
                 .birthDate(LocalDate.of(1953, 9, 2))
@@ -54,24 +42,14 @@ public class EmployeeRepositoryTest {
                 .gender(Gender.M)
                 .hireDate(LocalDate.of(1986, 6, 26))
                 .build();
-        title.setEmployee(employee);
-        salary.setEmployee(employee);
-        log.info("== Given complete ==");
 
         // When
         Employee savedEmployee = repository.save(employee);
-        log.info("== save complete ==");
         Employee foundEmployee = repository.findById(10001).orElse(null);
-        log.info("== When complete ==");
 
         // Then
         assertThat(foundEmployee).isNotNull()
                                  .isEqualTo(savedEmployee);
-        assertThat(foundEmployee.getSalaries()).contains(salary);
-        assertThat(foundEmployee.getTitles()).contains(title);
-
-        entityManager.flush();
-        log.info("== Then complete ==");
     }
 
 }
