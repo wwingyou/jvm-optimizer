b3fd0dbc46834d242017544d64e46b918d0525fe
wingyou <winwing.you@gmail.com>
2024-10-25 03:34:03 +0900

    feat(Department): adjust & implmement service

--- tester_v2/src/main/java/com/example/tester_v2/service/DepartmentService.java
+++ tester_v2/src/main/java/com/example/tester_v2/service/DepartmentService.java
@@ -3,7 +3,9 @@ package com.example.tester_v2.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.example.tester_v2.dto.DepartmentEmployeeForm;
 import com.example.tester_v2.dto.DepartmentForm;
+import com.example.tester_v2.dto.DepartmentManagerForm;
 import com.example.tester_v2.dto.DepartmentView;
 
 /**
@@ -17,9 +19,9 @@ public interface DepartmentService {
     DepartmentView getDepartmentByNumber(String departmentNumber);
     Page<DepartmentView> getDepartments(Pageable pageable);
 
-    void setEmployee(String departmentNumber, Integer employeeNumber);
+    void setEmployee(String departmentNumber, Integer employeeNumber, DepartmentEmployeeForm form);
     void deleteEmployee(String departmentNumber, Integer employeeNumber);
-    void setManager(String departmentNumber, Integer employeeNumber);
+    void setManager(String departmentNumber, Integer employeeNumber, DepartmentManagerForm form);
     void deleteManager(String departmentNumber, Integer employeeNumber);
 
 }
--- tester_v2/src/main/java/com/example/tester_v2/service/DepartmentServiceImpl.java
+++ tester_v2/src/main/java/com/example/tester_v2/service/DepartmentServiceImpl.java
@@ -4,67 +4,108 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.example.tester_v2.dto.DepartmentEmployeeForm;
 import com.example.tester_v2.dto.DepartmentForm;
+import com.example.tester_v2.dto.DepartmentManagerForm;
 import com.example.tester_v2.dto.DepartmentView;
+import com.example.tester_v2.entity.Department;
+import com.example.tester_v2.entity.DepartmentEmployee;
+import com.example.tester_v2.entity.DepartmentManager;
+import com.example.tester_v2.entity.Employee;
+import com.example.tester_v2.repository.DepartmentEmployeeRepository;
+import com.example.tester_v2.repository.DepartmentManagerRepository;
+import com.example.tester_v2.repository.DepartmentRepository;
+import com.example.tester_v2.repository.EmployeeRepository;
+
+import lombok.AllArgsConstructor;
 
 /**
  * DepartmentServiceImpl
  */
 @Service
+@AllArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentEmployeeRepository departmentEmployeeRepository;
+    private final DepartmentManagerRepository departmentManagerRepository;
+    private final EmployeeRepository employeeRepository;
 
     @Override
     public void createDepartment(DepartmentForm form) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createDepartment'");
+        var department = Department.builder()
+                .number(form.getDepartmentNumber())
+                .name(form.getDepartmentName())
+                .build();
+        departmentRepository.save(department);
     }
 
     @Override
     public void updateDepartment(DepartmentForm form) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateDepartment'");
+        var department = getDepartment(form.getDepartmentNumber());
+        department.setName(form.getDepartmentName());
+        departmentRepository.save(department);
     }
 
     @Override
     public void deleteDepartment(String departmentNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteDepartment'");
+        departmentRepository.deleteById(departmentNumber);
     }
 
     @Override
     public DepartmentView getDepartmentByNumber(String departmentNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDepartmentByNumber'");
+        return DepartmentView.of(getDepartment(departmentNumber));
     }
 
     @Override
     public Page<DepartmentView> getDepartments(Pageable pageable) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDepartments'");
+        return departmentRepository.findAll(pageable).map(DepartmentView::of);
     }
 
     @Override
-    public void setEmployee(String departmentNumber, Integer employeeNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setEmployee'");
+    public void setEmployee(String departmentNumber, Integer employeeNumber, DepartmentEmployeeForm form) {
+        var department = getDepartment(departmentNumber);
+        var employee = getEmployee(employeeNumber);
+        var departmentEmployee = DepartmentEmployee.builder()
+                .employee(employee)
+                .department(department)
+                .fromDate(form.getFromDate())
+                .toDate(form.getToDate())
+                .build();
+        departmentEmployeeRepository.save(departmentEmployee);
     }
 
     @Override
     public void deleteEmployee(String departmentNumber, Integer employeeNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteEmployee'");
+        departmentEmployeeRepository.deleteById(new DepartmentEmployee.PK(employeeNumber, departmentNumber));
     }
 
     @Override
-    public void setManager(String departmentNumber, Integer employeeNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setManager'");
+    public void setManager(String departmentNumber, Integer employeeNumber, DepartmentManagerForm form) {
+        var department = getDepartment(departmentNumber);
+        var manager = getEmployee(employeeNumber);
+        var departmentEmployee = DepartmentManager.builder()
+                .manager(manager)
+                .department(department)
+                .fromDate(form.getFromDate())
+                .toDate(form.getToDate())
+                .build();
+        departmentManagerRepository.save(departmentEmployee);
     }
 
     @Override
     public void deleteManager(String departmentNumber, Integer employeeNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteManager'");
+        departmentManagerRepository.deleteById(new DepartmentManager.PK(employeeNumber, departmentNumber));
+    }
+
+    private Department getDepartment(String departmentNumber) {
+        var department = departmentRepository.findById(departmentNumber)
+                .orElseThrow();
+        return department;
+    }
+    private Employee getEmployee(Integer employeeNumber) {
+        return employeeRepository.findById(employeeNumber)
+                .orElseThrow();
     }
 
 }
